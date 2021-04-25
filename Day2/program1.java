65a97cd9b94479a289c49447e362e27d0aaa1241
manpreetkaur15 <manpreetkaur0699@gmail.com>
2021-04-25 21:55:31 +0530

    jhhbj

---
+++ Day2/program1.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class program11{
+    public static void main(String[] args) {
+        int arr[]= new int[10];
+        int l,r,i;
+        Scanner sc= new Scanner(System.in);
+        for(i=0;i<10;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int sum=sc.nextInt();
+        for(i=0;i<10;i++){
+        l=i+1;
+        r=9;
+        while (l < r) 
+        {
+            if (arr[i] + arr[l] + arr[r] == sum) {
+                System.out.println("Triplets are: " + arr[i] + ", " + arr[l] + ", " + arr[r]);
+                System.exit(0);
+            } else if (arr[i] + arr[l] + arr[r] < sum)
+                l++;
+            else
+                r--;
+        }
+    }
+    System.out.println("The triplet is dosent exixt");
+        
+    }  
+}
