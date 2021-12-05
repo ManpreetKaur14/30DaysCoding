52efd14c8f6d64861471620c29635842295d8a79
manpreetkaur15 <manpreetkaur0699@gmail.com>
2021-12-06 01:32:56 +0530

    fibonacci with memoization

--- Manpreet/Day 5/memo.java
+++ Manpreet/Day 5/memo.java
@@ -1,26 +1,42 @@
 import java.util.*;
 
-class memo{
-// {
-//     static int count = 0;
-//     static int fib(int n)
-//     {
-//         count++;
-//         if (n == 0)
-//             return 0;
-//         if (n == 1)
-//             return 1;
-//         int sum = fib(n - 1) + fib(n - 2);
-//         return sum;
-//     }
-public static void main(String[] args)
+class memo 
+{
+    static int count = 0;
+    static  int arr[]=new int[10];
+    static int fib(int n)
     {
-    //     int s = fib(20);
-    // System.out.println(count);
-    int arr[]=new int[5];
-    Arrays.fill(arr,-1);
-
-    System.out.println(Arrays.toString(arr));
-    
+        count++;
+        int sum =0;
+           if(n==0 )
+           {
+            return 0;
+            }
+            else if(n==1)
+            {
+            arr[n]=1;
+            return arr[n];
+            } 
+        else if (arr[n] != 0)
+        {
+            return arr[n];
+        }
+            else{
+        sum = fib(n - 1) + fib(n - 2);
+        arr[n] = sum;
+        return sum;
+            }
+    }
+    public static void main(String[] args)
+    {
+        arr[0]=1;
+        arr[1]=1;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fib(n));
+        System.out.print(count);
     }
 }
+
+
+
