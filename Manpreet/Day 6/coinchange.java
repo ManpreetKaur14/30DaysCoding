2327b2f280b8e0602313c1a88120079257530da3
manpreetkaur15 <manpreetkaur0699@gmail.com>
2021-12-06 04:54:45 +0530

    corrected

--- Manpreet/Day 6/coinchange.java
+++ Manpreet/Day 6/coinchange.java
@@ -14,27 +14,23 @@ class coinchange
         }
         System.out.println("Enter the amount?");
         int amt = sc.nextInt();
-        int table[][]=new int[n][amt+1];
-        int m =0;
-        for(m=arr[m];m<n;m++)
+        int table[][]=new int[n+1][amt+1];
+        for(int m=0;m<=n;m++)
         {
-            for(int j=0;j<=amt;j++)
-            {
                 table[m][0]=1;
-            }
         }
         int i=0;
-        for(i=arr[i];i<n;i++)
+        for(i=1;i<=n;i++)
         {
-            for(int j=0;j<=amt;j++)
+            for(int j=1;j<=amt;j++)
             {
-                if(arr[i]>j)
+                if(arr[i-1]>j)
                 table[i][j]=table[i-1][j];
                 else
-                table[i][j]=table[i-1][j]+table[i][j-arr[i]];
+                table[i][j]=table[i-1][j]+table[i][j-arr[i-1]];
             }
         }
-          System.out.println(Arrays.deepToString(table));
+        System.out.println(table[n][amt]);
         
     }
 }
