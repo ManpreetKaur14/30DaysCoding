4186030eaae124283e907f2f5f8a0c68c04a112b
manpreetkaur15 <manpreetkaur0699@gmail.com>
2021-12-05 23:57:51 +0530

    memo

---
+++ Manpreet/Day 5/memo.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class memo{
+// {
+//     static int count = 0;
+//     static int fib(int n)
+//     {
+//         count++;
+//         if (n == 0)
+//             return 0;
+//         if (n == 1)
+//             return 1;
+//         int sum = fib(n - 1) + fib(n - 2);
+//         return sum;
+//     }
+public static void main(String[] args)
+    {
+    //     int s = fib(20);
+    // System.out.println(count);
+    int arr[]=new int[5];
+    Arrays.fill(arr,-1);
+
+    System.out.println(Arrays.toString(arr));
+    
+    }
+}
